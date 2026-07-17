a99c2be2d250a68fcfa9f59ca098c8858cb5c5da
Aditya Sharma <adityasharma10@amityonline.com>
2026-07-17 10:59:17 +0530

    Increase character image size by 100% and completely remove floating animation to lock it in place

--- src/components/ErrorRobot.jsx
+++ src/components/ErrorRobot.jsx
@@ -1,22 +1,12 @@
-import { motion } from "framer-motion";
 import Robot from "../assets/error/robot.png";
 
 export default function ErrorRobot() {
   return (
-    <motion.img
+    <img
       src={Robot}
       alt="404 Robot"
-      className="relative z-20 w-[450px] sm:w-[580px] md:w-[720px] lg:w-[860px] xl:w-[1000px] select-none"
+      className="relative z-20 w-[600px] sm:w-[800px] md:w-[950px] lg:w-[1100px] xl:w-[1300px] select-none"
       draggable={false}
-      animate={{
-        y: [0, -10, 0],
-        rotate: [-1, 1, -1]
-      }}
-      transition={{
-        duration: 4,
-        repeat: Infinity,
-        ease: "easeInOut"
-      }}
     />
   );
 }
